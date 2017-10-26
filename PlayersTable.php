010c3fb9b315f133c645a8a66632b71004a2f275
RomainGirard2 <31858686+RomainGirard2@users.noreply.github.com>
2017-10-26 23:33:37 +0200

    Add files via upload

---
+++ PlayersTable.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class PlayersTable extends Table{
+    
+    public function add_Players($email,$password){
+        $a=$this->newEntity();
+        $a->email=$email;
+        $a->password=$password;
+        $this->save($a);
+    }
+    
+    public function check_Players($data){
+        $a=$this->newEntity();
+        $data=$this->find('all')
+            ->where(['email'=>$data['email']])
+            ->first();
+        if($data){
+            $a=$data->toArray();
+            return $a;
+        }
+    }
+    
+    public function change_Password($id,$newPass){
+        $a=$this->get($id);
+        $a->password=$newPass;
+        $this->save($a);
+    }
+}
